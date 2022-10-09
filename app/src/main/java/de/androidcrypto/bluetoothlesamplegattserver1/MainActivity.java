de89705f784ba9ce1671286cf2052e62223a7f49
michaelfehr <michael_fehrgmx.de>
2022-10-09 20:19:09 +0200

    updated

--- app/src/main/java/de/androidcrypto/bluetoothlesamplegattserver1/MainActivity.java
+++ app/src/main/java/de/androidcrypto/bluetoothlesamplegattserver1/MainActivity.java
@@ -1,14 +1,58 @@
 package de.androidcrypto.bluetoothlesamplegattserver1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * This block is for requesting permissions up to Android 12+
+     *
+     */
+
+    private static final int PERMISSIONS_REQUEST_CODE = 191;
+    private static final String[] BLE_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+    };
+    @SuppressLint("InlinedApi")
+    private static final String[] ANDROID_12_BLE_PERMISSIONS = new String[]{
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_ADVERTISE,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    public static void requestBlePermissions(Activity activity, int requestCode) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            ActivityCompat.requestPermissions(activity, ANDROID_12_BLE_PERMISSIONS, requestCode);
+        else
+            ActivityCompat.requestPermissions(activity, BLE_PERMISSIONS, requestCode);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestBlePermissions(this, PERMISSIONS_REQUEST_CODE);
+
+        Button startServer = findViewById(R.id.btnStartServer);
+        startServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GattServerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
